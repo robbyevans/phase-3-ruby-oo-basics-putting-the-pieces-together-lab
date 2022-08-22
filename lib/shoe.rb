89eff0611f7e8bd328d989f658549827d646c5cf
Evans Rutto <robbyevans001@gmail.com>
2022-08-22 12:21:19 +0300

    commit tree 4834d919064e57ff1e1a6380c6f6af4038144d75

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,22 @@
 # Make your shoe class here!
+
+
+class Shoe
+  attr_accessor :brand,:brand,:color,:size,:material,:condition
+  def initialize(brand)
+    @brand=brand
+  end
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition="new"
+  end
+end
+
+ boots=Shoe.new("addidas")
+
+#  pp boots.name
+#  pp boots.size=12
+#  pp boots.color="green"
+#  pp boots.material="leather"
+#  pp boots.condition="new"
+#  pp boots.brand="Nike"
